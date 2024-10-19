8df7c2caedcfccb3bf52165bcb9c2e80f1323af6
Võ Minh Phú <vominhphu2003@gmail.com>
2024-10-19 22:56:25 +0700

    ADD: shadcn , custom button, input, select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive" size="lg" >hello world! 123</Button>
+    <div className="gap-4 flex items-center justify-center">
+      <Input />
+      <Button size="sm">Primary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="muted">muted</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="teritary">teritrary</Button>
     </div>
   );
 }
