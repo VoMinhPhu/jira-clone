1ca4bdec53f718a24a2bb40101ba1d830f770c41
Võ Minh Phú <vominhphu2003@gmail.com>
2024-10-21 23:55:37 +0700

    ADD: sign in page and sign up page

---
+++ src/components/dotted-separator.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+
+interface DottedSeparatorProps {
+    clasName?: string;
+    color?: string;
+    height?: string;
+    dotsize?: string;
+    gapSize?: string;
+    direction?: "horizontal" | "vertical";
+}
+
+export const DottedSeparator = ({
+    clasName,
+    color = "#d4d4d8",
+    height = "2px",
+    dotsize = "2px",
+    gapSize = "6px",
+    direction = "horizontal"
+}: DottedSeparatorProps) => {
+    const isHorizontal = direction === "horizontal";
+
+    return (
+        <div className={cn(
+            isHorizontal ? "w-full flex items-center" : "h-full flex flex-col items-center",
+            clasName,
+        )}>
+            <div
+                className={isHorizontal ? "flex-grow" : "flex-grow-0"}
+                style={{
+                    width: isHorizontal ? "100%" : height,
+                    height: isHorizontal ? height : "100%",
+                    backgroundImage: `radial-gradient(circle,${color} 25%, transparent 25%)`,
+                    backgroundSize: isHorizontal
+                        ? `${parseInt(dotsize) + parseInt(gapSize)}px ${height}`
+                        : `${height} ${parseInt(dotsize) + parseInt(gapSize)}px`,
+                    backgroundRepeat: isHorizontal ? "repeat-x" : "repeat-y",
+                    backgroundPosition: "center",
+                }}
+            />
+        </div>
+    )
+}
